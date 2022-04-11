93d6161656ecb42e8af39f7f3c4179ff560e4ff3
ronick <roromulus22@gmail.com>
2022-04-10 20:09:50 -0400

    deploy to heroku

---
+++ server.js
@@ -0,0 +1,1 @@
+const express = require('express');const path = require('path');const app = express();app.use(express.static(__dirname + '/dist/iri'));app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/iri/index.html'));});app.listen(process.env.PORT || 8080);
